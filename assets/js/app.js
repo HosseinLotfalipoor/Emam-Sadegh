f2b9ea0b867c6727614dea03d17cbf8bfa51e581
HosseinLotfalipoor <lotfalipoorhossein915@gmail.com>
2025-01-04 20:41:19 +0330

    resposive index

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,34 @@
-
 // honor card background color
 
 const honorCards = document.querySelectorAll(".honors-card");
 const iconContainer = document.querySelectorAll(".fa-chevron-left");
+const moblieNav = document.querySelector(".moblie-nav");
+const headerNav = document.querySelector(".header-nav");
+const ctrlBtn = document.querySelector(".ctrl-btn");
+const body = document.body;
+const sideBar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
 
-for (var card in honorCards) {
-  honorCards[card].classList.add(`card-${Number(card) + 1}`);
-  iconContainer[card].classList.add(`card-${Number(card) + 1}`);
+for (var i = 0;i < honorCards.length;i++) {
+  honorCards[i].classList.add(`card-${Number(i) + 1}`);
+  iconContainer[i].classList.add(`card-${Number(i) + 1}`);
 }
+
+if (innerWidth <= 768) {
+    moblieNav.style.display = "";
+    headerNav.style.display = "none";
+} else {
+    moblieNav.style.display = "none";
+    headerNav.style.display = "";
+}
+
+ctrlBtn.addEventListener("click", () => {
+    body.classList.toggle("layer-body", "hidden-scroll");
+    sideBar.classList.toggle("show");
+    document.documentElement.classList.toggle("hidden-scroll");
+});
+closeBtn.addEventListener("click", () => {
+    body.classList.remove("layer-body", "hidden-scroll");
+    sideBar.classList.toggle("show");
+    document.documentElement.classList.toggle("hidden-scroll");
+});
